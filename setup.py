5a31e50e33f7ebc04ade15e905087bb4aa7609da
PaulLundgren <44200079+PaulLundgren@users.noreply.github.com>
2021-07-30 16:29:26 -0400

    Changed setup to allow installer creation

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
-import setuptools
+import sys
+from cx_Freeze import setup, Executable
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+if sys.platform == "win64":
+    base = "Win64GUI"
 
-setuptools.setup(
+setup(
     name='ProjectRun',
     version='0.5.0',
     description='2D side scroller written in python with pygame',
     author='Paul Lundgren, Conner Gallimore, Martin Lasprilla, Lunafreya Nguyen',
     url='https://github.com/PaulLundgren/Project-Run',
-    packages=['GameFiles',],
+    packages=['GameFiles'],
     long_description=long_description,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
     ],
     entry_points={
         'console_scripts': [
@@ -22,7 +27,15 @@ setuptools.setup(
         ],
     },
     install_requires=[
-        'pygame~=2.0.1'
+        'pygame~=2.0.1',
+        'cx-Freeze~=6.7',
     ],
-    include_package_data=True
+    include_package_data=True,
+    executables=[Executable("GameFiles/ProjectRun.py",
+                            base=base,
+                            targetName="ProjectRun.exe",
+                            shortcutName="ProjectRun",
+                            shortcutDir="DesktopFolder",
+                            icon="GameFiles/Images/icon.ico")]
 )
+
